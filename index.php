c053ba564aece84fa5de18a5aa024a1226e557f8
firzafaathir-collab <firzafaathir@gmail.com>
2026-02-26 09:49:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Biodata diri</title>
+</head>
+<body>
+	<div style="text-align: center;">
+		<img src="latar biru.JPEG" alt="" width="100px">
+</div>
+
+	<h1 style="text-align: center;">BIODATA DIRI</h1>
+	<p style="text-align: center; color: brown;">muhammad faathir firzah</p>
+	<p>nama saya muhammad faathir firzah, sering dipangil fathir, saya berasal dari kabupaten tolitoli sulawesi tengah. saya berusia 19 tahun dan kini saya berkuliah di universitas hasanuddin jurusan terapi gigi dan mulut angkatan 2024<p>
+        <footer>
+    <hr> <p><strong>Alamat:</strong> Jl. perjuangan, tamalanrea kota makassar sulawesi selatan</p>
+</footer>
